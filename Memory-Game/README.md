c5422e272599fb0ad08a3e3b30b99875ef158712
Alex <99199668+TorrijaConLeche@users.noreply.github.com>
2023-05-11 15:43:34 +0000

    Adding Memory Game

---
+++ Memory-Game/README.md
@@ -0,0 +1,32 @@
+## Epic Memory Game
+
+Typical memory game with cards. I struggled a lot with this one, enjoy it :D
+
+You can view and example [here](https://java-script-projects-ten.vercel.app/Memory-Game)
+
+Project idea by [Andy Sterkowitz](https://www.youtube.com/watch?v=roumzWd4XJU)
+
+---
+
+### Aspects to consider:
+
+1. You will create a button to randomize cards so the user can play a new game.
+
+2. Use the number of rows and columns that you want (recommend you grid).
+
+3. The user can see the amount of failed and successful attempts.
+
+4. The user will be notified if he wins.
+
+5. Don't copy my code, I'm just a noobie :D
+
+### Ideas to make it more complex:
+
+- Add more cards to the game
+
+- Add winning effect i.e confetti library
+
+- Loading bar while randomizing
+
+- Add difficulty levels
+
---
+++ Memory-Game/main.js
@@ -0,0 +1,123 @@
+
+let mainDiv = document.getElementById("game")
+let elements = mainDiv.querySelectorAll("div")
+
+let imageList = []     // Array para almacenar la url de las imagenes y compararlas
+let imageIds = []       // Array de ids que identifican cada img
+
+let tries = 0 // Times user click on card
+let fails = 0 // Times user select cards that dont match
+let wins = 0
+
+let failstext = document.getElementById("failstext")
+let winstext = document.getElementById("winstext")
+
+
+for (let i = 0, len = elements.length; i < len; i++) {
+
+
+
+    elements[i].onclick = checkImages
+
+    function checkImages() {
+
+        let image = elements[i].querySelector("img") // IMG ELEMENTS
+
+        let imageAttr = image.getAttribute("src") // Store img attributes to compare
+        let imgId = image.getAttribute("id")
+
+        imageList.push(imageAttr)
+
+        imageIds.push(imgId)
+
+        console.log("A", imageList)
+
+        image.setAttribute("style", "opacity: 1;")
+
+        tries += 1
+        console.log("Tries: ", tries)
+
+        if (tries === 2) {
+
+            if (imageList[0] === imageList[1]) {
+                setWins()
+            }
+            else {
+                setTimeout(setFails, 100)
+                fails += 1
+                failstext.innerHTML = `Failed attempts: ${fails}`
+            }
+        }
+    }
+}
+
+function setFails() {
+    // Hide cards again and reset the game
+    document.getElementById(imageIds[0]).setAttribute("style", "transition: all 1.2s ease-in-out; opacity: 0;")
+    document.getElementById(imageIds[1]).setAttribute("style", "transition: all 1.2s ease-in-out; opacity: 0;")
+    reset()
+}
+
+function setWins() {
+    wins += 1
+    winstext.innerHTML = `Successful attempts: ${wins}`
+    reset()
+}
+
+function reset() {
+    imageList = []     // Array para almacenar la url de las imagenes y compararlas
+    imageIds = []       // Array de ids que identifican cada img
+    tries = 0 // Times user click on card
+
+}
+
+let randomize = document.getElementById("randomize")
+
+randomize.onclick = () => randomizeCards()
+
+function randomizeCards() {
+
+    let numArray = createArray() // Define Array of numbers
+
+    for (let i = 0, len = elements.length; i < len; i++) {
+        let image = elements[i].querySelector("img")
+        image.setAttribute('src', `content/${numArray[i]}.png`)
+        image.setAttribute('style', "opacity: 0;")
+        console.log(image)
+    }
+
+    reset()
+    winstext.innerHTML = ""
+    failstext.innerHTML = ""
+    fails = 0 // Times user select cards that dont match
+    wins = 0
+
+}
+
+
+// Create array with 3 pairs of numbers i.e [1, 0, 2, 0, 2, 1]
+function createArray() {
+    let numArray = []
+    while (numArray.length < 6) {
+        let random = Math.floor(Math.random() * 3)
+        let contador = countInArray(numArray, random)
+
+        if (contador != 2) {
+            numArray.push(random)
+        }
+    }
+    return numArray
+}
+
+
+// Function to count concurrencies on the Array
+function countInArray(array, what) {
+    var count = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === what) {
+            count++;
+        }
+    }
+    return count;
+}
+
