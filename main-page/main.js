29d341a6eda375134b03f1791a5f820ca644e4a2
Alex <99199668+TorrijaConLeche@users.noreply.github.com>
2023-05-19 15:13:16 +0000

    Adding sort and filter options to the main page

--- main-page/main.js
+++ main-page/main.js
@@ -1,54 +1,72 @@
-const projects = {
-    1: {
-        name: "To Do App",
-        image: "/main-page/content/to-do-app.png",
-        url: "/To-Do-App"
-    },
-    2: {
-        name: "Vowel Counter",
-        image: "/main-page/content/vowel-counter.png",
-        url: "/Vowel-Counter"
-    },
-    3: {
-        name: "Memory Game",
-        image: "/main-page/content/memory-game.png",
-        url: "/Memory-Game"
-    },
-    4: {
-        name: "Hangman Game",
-        image: "/main-page/content/hangman-game.png",
-        url: "/Hangman-Game"
-    },
-    5: {
-        name: "Coming Soon",
-        image: "/main-page/content/soon.jpg",
-        url: "/soon"
-    },
-    6: {
-        name: "Coming Soon",
-        image: "/main-page/content/soon.jpg",
-        url: "/soon"
-    },
-}
-
+import { projects, toggleSort } from '/main-page/projects.js'
 let maindiv = document.getElementById("container") // Select main div which contains the projects
 
-console.log(maindiv)
+let used_projects = projects
+
+let filter = document.getElementById("filter") // Input type range
+filter.value = 0
+
+showProjects(filter.value) // Initialize
+
+let sortButton = document.getElementById("sort")
+
+sortButton.onclick = () => { // Button to sort projects
+
+    used_projects = toggleSort() // Toggle dict with projects
+    maindiv.innerHTML = "" // Clear projects from the main page
+    showProjects(filter.value) // Show projects in new order
+}
+
+let rangevalue = document.getElementById("rangevalue") // Text to show the value
+
+filter.oninput = (event) => { // Trigger when range changes
+    rangevalue.innerHTML = filter.value
+    maindiv.innerHTML = ""
+    showProjects(filter.value)
+}
 
 
 
-for (let i in projects) {
+// Main function to show projects
+function showProjects(diffvalue) {
 
-    let eachdiv = document.createElement("DIV") // Create div for each project
-    eachdiv.setAttribute("id", "eachdiv")
-    eachdiv.setAttribute("onclick", `window.location='${projects[i].url}'`) // Add link to the project
+    for (let i in used_projects) {
+        if (used_projects[i].diff >= diffvalue) {
+            let eachdiv = document.createElement("DIV") // Create div for each project
+            eachdiv.setAttribute("id", "eachdiv")
+            eachdiv.setAttribute("onclick", `window.location='${used_projects[i].url}'`) // Add link to the project
 
-    let eachtext = document.createElement("SPAN") // Create text for each project
-    eachtext.innerHTML = projects[i].name
+            let eachtext = document.createElement("SPAN") // Create text for each project
+            eachtext.innerHTML = used_projects[i].name
 
-    let eachimage = document.createElement("IMG") // Create image for each project
-    eachimage.setAttribute("src", projects[i].image)
+            let eachimage = document.createElement("IMG") // Create image for each project
+            eachimage.setAttribute("src", used_projects[i].image)
 
-    eachdiv.append(eachimage, eachtext) // Append elements
-    maindiv.appendChild(eachdiv)
+            // Background depending on the project difficulty
+
+            let bg = ""
+
+            let difficulty = document.createElement("DIV")
+            let eachdiff = document.createElement("DIV")
+
+            if (used_projects[i].diff < 33) {
+                bg = `linear-gradient(90deg, rgba(110,255,61,${used_projects[i].diff * 0.03}) 0%, rgba(241,255,0,${used_projects[i].diff * 0.01}) 33%, rgba(255,255,255,1) 100%)`
+            }
+            else if (used_projects[i].diff < 66 && used_projects[i].diff > 32) {
+                bg = `linear-gradient(90deg,rgba(110,255,61,1) 0%, rgba(241,255,0,${used_projects[i].diff * 0.0152}) 50%, rgba(255,0,0,${used_projects[i].diff * 0.002}) 66%, rgba(255,255,255,1) 100%)`
+            }
+            else if (used_projects[i].diff > 66) {
+                bg = `linear-gradient(90deg, rgba(110,255,61,1) 0%, rgba(241,255,0,1) 50%, rgba(255,0,0,${used_projects[i].diff * 0.006}) 100%)`
+            }
+
+            eachdiff.setAttribute("style", `background: ${bg};`)
+
+            difficulty.append(eachdiff)
+            difficulty.setAttribute("class", "difficulty")
+
+            eachdiv.append(eachimage, difficulty, eachtext) // Append elements
+            maindiv.appendChild(eachdiv)
+        }
+
+    }
 }
---
+++ main-page/projects.js
@@ -0,0 +1,86 @@
+export let projects = {
+    1: {
+        name: "To Do App",
+        image: "/main-page/content/to-do-app.png",
+        url: "/To-Do-App",
+        diff: 20
+    },
+    2: {
+        name: "Vowel Counter",
+        image: "/main-page/content/vowel-counter.png",
+        url: "/Vowel-Counter",
+        diff: 15
+    },
+    3: {
+        name: "Memory Game",
+        image: "/main-page/content/memory-game.png",
+        url: "/Memory-Game",
+        diff: 40
+    },
+    4: {
+        name: "Hangman Game",
+        image: "/main-page/content/hangman-game.png",
+        url: "/Hangman-Game",
+        diff: 32
+    },
+    5: {
+        name: "Coming Soon",
+        image: "/main-page/content/soon.jpg",
+        url: "/soon",
+        diff: 0
+    },
+    6: {
+        name: "Coming Soon",
+        image: "/main-page/content/soon.jpg",
+        url: "/soon",
+        diff: 0
+    },
+}
+
+
+export function sort_projects(arg) {
+
+    // Create items array
+    var items = Object.keys(projects).map(function (key) {
+        return [key, projects[key]];
+    });
+
+    // Sort the array based on the second element
+    items.sort(function (first, second) {
+        if (arg === "less") {
+            return first[1].diff - second[1].diff // order from least difficult to most difficult
+        }
+        else {
+            return second[1].diff - first[1].diff // order from most difficult to least difficult
+        }
+
+    });
+    //  Items now are sorted
+
+    let sorted_projects = {} // Sorted_projects will store projects from less to more 
+
+    items.forEach(function (k, v) { // Iterate the array to create the new dict with sorted values
+        let use_key = v // key
+        let use_value = k[1] // value
+        sorted_projects[use_key] = use_value
+    })
+
+    return sorted_projects
+}
+
+
+let alter = false
+export function toggleSort() {
+    {
+        if (alter === false) {
+            alter = true
+            return sort_projects("more")
+
+        }
+        else {
+            alter = false
+            return sort_projects("less")
+
+        }
+    }
+}
