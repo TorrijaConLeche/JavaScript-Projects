a9ddfef72c5961472a9a83311083869784d5bded
Alex <99199668+TorrijaConLeche@users.noreply.github.com>
2023-05-29 14:08:58 +0000

    Adding copy style features

--- card-generator/custom.js
+++ card-generator/custom.js
@@ -1,7 +1,6 @@
 
 // SELECT ALL INPUTS IN THE DOCUMENT
 let allInputs = document.querySelectorAll("input")
-console.log(allInputs)
 
 for (let i = 0; i < allInputs.length; i++) {
     allInputs[i].addEventListener("input", updateStyles, false)
@@ -9,6 +8,164 @@ for (let i = 0; i < allInputs.length; i++) {
 
 
 updateStyles()
+// Create var to store current styles and show them to the user
+
+
+// Code copy to clipboard
+csscode.onclick = () => {
+    copyCSS()
+}
+
+function copyCSS() {
+    let codeCSS = `
+body {
+    background: radial-gradient(50% 0%, rgba(1, 3, 76, 0.982) 1%, rgb(21, 23, 42) 50%);
+    color: rgb(221, 221, 221);
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    font-family: system-ui;
+}
+
+#card {
+    background-color: ${bgcolor.value};
+    box-shadow: ${boxshadowx.value}px ${boxshadowy.value}px ${boxshadowblur.value}px ${boxshadowsize.value}px ${boxshadowcolor.value};
+    width: 95%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    place-self: center;
+    padding: 1%;
+    padding-bottom: 20px;
+    padding-top: 3%;
+}
+
+#cardbg {
+    width: ${widthrange.value / 2}%;
+    border: ${bordersize.value}px solid ${bordercolor.value};
+    border-radius: ${borderadius.value}px;
+    padding: 6px 1px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+}
+
+#imagediv img {
+    width: 280px;
+    height: 280px;
+}
+
+#info {
+    display: flex;
+    width: 100%;
+}
+
+#types {
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    gap: 13px;
+    height: auto;
+    padding: 5px;
+}
+#types span {
+    background-color: aaaaaaaaaaaa;
+    color: aa;
+    padding: 4px 18px;
+    text-align: center;
+    place-self: center;
+    font-size: 16px;
+    font-weight: 700;
+    border-radius: 6px;
+    box-shadow: 3px 3px 0px rgb(255, 255, 255);
+}
+
+#moves {
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-content: center;
+    align-items: center;
+    gap: 13px;
+    width: 50%;
+    height: auto;
+    padding: 5px;
+}
+
+#moves span {
+    background-color: rgb(21, 26, 22);
+    padding: 4px 18px;
+    text-align: center;
+    place-self: center;
+    font-size: 16px;
+    font-weight: 700;
+    border-radius: 6px;
+    box-shadow: 3px 3px 0px rgb(255, 255, 255);
+    margin: 0;
+}
+
+#moves h3 {
+    margin: 8px;
+}
+
+#stats {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    justify-content: center;
+    gap: 20px 10px;
+    margin-top: 20px;
+    margin-bottom: 10px;
+}
+
+#poketextname {
+    color: ${pokenamecolor.value};
+    font-size: ${pokenamesize.value}px;
+}
+
+.statcontent {
+    display: flex;
+    justify-content: center;
+    gap: 3%;
+}
+
+.statcontent span {
+    width: 45px;
+    font-weight: 500;
+}
+
+.stat {
+    background-color: ${statbgcolor.value};
+    width: 100px;
+    place-self: center;
+    display: flex;
+}
+
+.statext {
+    color: ${statextcolor.value};
+    font-size: ${statnamesize.value}px;
+}
+
+.statbar {
+    background-color: ${statbarcolor.value};
+    place-content: center;
+    width: 110px;
+    height: 10px;
+}
+`
+    navigator.clipboard.writeText(codeCSS)
+    copiedtext.setAttribute("style", "display: inline; opacity: 1; transition: all .2s ease-in-out")
+    setTimeout(clearText, 2000)
+    function clearText() {
+        copiedtext.setAttribute("style", "opacity: 0; transition: all .2s ease-in-out")
+    }
+}
+
 
 function updateStyles() {
 
@@ -36,18 +193,22 @@ function updateStyles() {
     let statext = document.getElementsByClassName("statext")
 
     for (let i = 0; i < statbars.length; i++) {
+
         let wide = statbars[i].getAttribute("style")
-        statbars[i].setAttribute("style", `
-        ${wide}
-        background-color: ${statbarcolor.value};
-        `)
+        if (wide != null) {
+            statbars[i].setAttribute("style", `${wide.split(";")[0]}; background-color: ${statbarcolor.value};
+            `)
+        }
+
 
         let wideBG = statbarsBG[i].getAttribute("style")
+        statbarsBG[i].removeAttribute("style")
         statbarsBG[i].setAttribute("style", `
         ${wideBG}
         background-color: ${statbgcolor.value};
         `)
 
+        statext[i].removeAttribute("style")
         statext[i].setAttribute("style", `
         color: ${statextcolor.value};
         font-size: ${statnamesize.value}px;
--- card-generator/main.js
+++ card-generator/main.js
@@ -31,15 +31,16 @@ pokeButton.onclick = async function () {
 
 getPokemon(`${Math.floor(Math.random() * 1000)}`)
 
+let pokeImg = ""
 async function getPokemon(pokeName) {
 
     let API_URL = `https://pokeapi.co/api/v2/pokemon/${pokeName}`
     let res = await fetch(API_URL)
     let pokemon = await res.json()
-    console.log(pokemon)
 
 
-    let pokeImg = pokemon.sprites.other.home.front_default
+
+    pokeImg = pokemon.sprites.other.home.front_default
     let pokeTypes = pokemon.types
     let pokeMoves = pokemon.abilities
     let pokeStats = pokemon.stats
@@ -48,6 +49,8 @@ async function getPokemon(pokeName) {
     updateStats(pokeStats)
 }
 
+let spanCollection = ""
+let moveList = ""
 function updateCard(pokeName, pokeImg, pokeTypes, pokeMoves) {
     let pName = document.getElementById("poketextname")
     let image = document.getElementById("image")
@@ -60,10 +63,15 @@ function updateCard(pokeName, pokeImg, pokeTypes, pokeMoves) {
     pName.innerHTML = pokeName
     image.setAttribute("src", pokeImg) // Update image
 
+    // collect spans
+
+
+
     // Iterate through the types array
     for (let i of pokeTypes) {
         let type = toCapital(i.type.name) // Define type with 1st Capital
         let newtype = document.createElement("SPAN")
+        newtype.className = "typespan"
         newtype.style.background = "#" + typeColors[type]
         if (type === "Dragon" || type === "Dark") {
             newtype.style.color = "white"
@@ -71,8 +79,12 @@ function updateCard(pokeName, pokeImg, pokeTypes, pokeMoves) {
             newtype.style.color = "black"
         }
 
+
         newtype.innerHTML = type
         types.appendChild(newtype) // Append element with type to the cards
+
+        spanCollection = newtype.parentNode.innerHTML
+
     }
 
     // Iterate through the moves array
@@ -82,8 +94,111 @@ function updateCard(pokeName, pokeImg, pokeTypes, pokeMoves) {
 
         newmove.innerHTML = move
         moves.appendChild(newmove)
+
+        moveList = newmove.parentNode.innerHTML
     }
 }
+
+htmlcode.onclick = () => {
+    copyHTML(spanCollection)
+
+
+}
+
+function copyHTML(spanList) {
+
+
+    copiedtext.setAttribute("style", "display: inline; opacity: 1; transition: all .2s ease-in-out")
+    setTimeout(clearText, 2000)
+    function clearText() {
+        copiedtext.setAttribute("style", "opacity: 0; transition: all .2s ease-in-out")
+    }
+
+    let pokeN = document.getElementById("poketextname").innerHTML
+
+    let bars = document.getElementsByClassName("statbar")
+    let barElements = []
+    for (let i = 0; i < bars.length; i++) {
+
+        barElements.push(bars[i].getAttribute("style"))
+    }
+
+
+    let codeHTML = `
+    <div id="cardbg">
+    <div id="card">
+
+        <div id="imagediv">
+            <img id="image" src="${pokeImg}">
+        </div>
+
+        <h2 id="poketextname">${pokeN}</h2>
+
+        <div id="info">
+
+            <div id="types">
+            ${spanList}
+            </div>
+
+            <div id="moves">
+            ${moveList}
+            </div>
+
+        </div>
+        <div id="stats">
+            <div class="statcontent">
+                <span class="statext">HP</span>
+                <div class="stat">
+                    <div id="HP" class="statbar" style="${barElements[0]}"></div>
+                </div>
+            </div>
+
+            <div class="statcontent">
+                <span class="statext">ATT</span>
+                <div class="stat">
+                    <div id="ATT" class="statbar" style="${barElements[1]}"></div>
+                </div>
+            </div>
+            <div class="statcontent">
+                <span class="statext">DEF</span>
+                <div class="stat">
+                    <div id="DEF" class="statbar" style="${barElements[2]}"></div>
+                </div>
+            </div>
+
+            <div class="statcontent">
+                <span class="statext">S.ATT</span>
+                <div class="stat">
+                    <div id="S.ATT" class="statbar" style="${barElements[3]}"></div>
+                </div>
+            </div>
+
+            <div class="statcontent">
+                <span class="statext">S.DEF</span>
+                <div class="stat">
+                    <div id="S.DEF" class="statbar" style="${barElements[4]}"></div>
+                </div>
+            </div>
+
+            <div class="statcontent">
+                <span class="statext">SPD</span>
+                <div class="stat">
+                    <div id="SPD" class="statbar" style="${barElements[5]}"></div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+    
+    `
+    navigator.clipboard.writeText(codeHTML)
+    console.log(codeHTML)
+}
+
+
+
 
 function updateStats(pokeStats) { // Max 255
     pokeStats[0].stat.name = "HP"
@@ -96,7 +211,8 @@ function updateStats(pokeStats) { // Max 255
     for (let i of pokeStats) {
         let statbar = document.getElementById(i.stat.name)
         let size = (i.base_stat / 255) * 100
-        statbar.setAttribute("style", `width: ${size}%;`)
+        statbar.setAttribute("style", `width: ${size}%; background-color: #5f0873;`)
+
     }
 
 }
